18c5b9451be013cc2023cafb070a2843caaff81d
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-11 11:42:06 +0300

    Поправил валидацию данных utils/validatos (Joi)

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,15 +2,15 @@ const movieRoutes = require('express').Router();
 
 // eslint-disable-next-line object-curly-newline
 const { getMovies, deleteMovie, createMovie } = require('../controllers/card');
-const { validateCardId, validateCard } = require('../utils/validators/cardValidator');
+const { validateMovieId, validateMovie } = require('../utils/validators/movieValidator');
 
 // возвращает все сохранённые текущим  пользователем фильмы
 movieRoutes.get('/', getMovies);
 // создаёт фильм с переданными в теле
 // country, director, duration, year, description, image,
 // trailer, nameRU, nameEN и thumbnail, movieId
-movieRoutes.post('/', validateCard, createMovie);
+movieRoutes.post('/', validateMovie, createMovie);
 // удаляет сохранённый фильм по id
-movieRoutes.delete('/:movieId', validateCardId, deleteMovie);
+movieRoutes.delete('/:movieId', validateMovieId, deleteMovie);
 
 module.exports = movieRoutes;
---
+++ utils/constants.js
@@ -0,0 +1,7 @@
+module.exports.DEFAULT_SUCCESS_CODE = 200;
+module.exports.SUCCESS_CREATED_CODE = 201;
+module.exports.INCORRECT_DATA_ERROR_CODE = 400;
+module.exports.FORBIDDEN_ERROR_CODE = 403;
+module.exports.NOT_FOUND_ERROR_CODE = 404;
+module.exports.DEFAULT_ERROR_CODE = 500;
+module.exports.URL_REGEXP = /^https?:\/\/[^\s/$.?#].[^\s]*$/i;
